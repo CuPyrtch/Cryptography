7d22e3e59ab893e72ff3edc8ed08177182e6d041
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-04-03 20:58:10 -0400

    Updated from Brython Server: 4/3/2019 8:58:09 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,23 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+encrypt = ""
+while encrypt != "q":
+    encrypt = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if encrypt == "e":
+        message = input("Message: ")
+        key = input("Key: ")
+        for i in range(0, len(message)):
+            index = associations.find(message[i])
+            print(message[i] + " " + str(index))
+        
+    elif encrypt == "d":
+        message = input("Message: ")
+        key = input("Key: "
+    
+    elif encrypt == "q":
+        print("Goodbye!")
+        
+    else:
+        print("Did not understand command, try again.")
+        
